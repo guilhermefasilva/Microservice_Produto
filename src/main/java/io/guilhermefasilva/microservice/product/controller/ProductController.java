4100b663bf2c344798e5ce067042871c0f65b588
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-08-18 18:02:31 -0300

    Alterando a documentação dos endpoints, padronizando as annotations e possiveis respostas http

--- src/main/java/io/guilhermefasilva/microservice/product/controller/ProductController.java
+++ src/main/java/io/guilhermefasilva/microservice/product/controller/ProductController.java
@@ -43,22 +43,11 @@ public class ProductController {
 	
 	@PostMapping
 	@Transactional
-	@ApiOperation(
-				httpMethod = "POST",
-				value = "Cadastrar um produto na base de dados",
-				response = ProductDtoResponse.class,
-				nickname = "createProduct",
-				notes = "Os valores devem ser fornecidos no corpo da requisição em formato Json, com excessão do campo preco que "
-						+ "\n por padrão é formato numérico, os demais campos são strings. O campo Status é um tipo enum e aceita"
-						+ "\n somente dois valores, ATIVO e INATIVO."
-			
-			
-			)
+	@ApiOperation(value = "Realiza o cadastro de produtos")
 	@ApiResponses(value = {
-						@ApiResponse(code = 201, message = "Created Resource, requisição bem sucedida"),
-						@ApiResponse(code = 400, message = "Bad Request, requisição não atendida ")
-	}	
-			)
+						@ApiResponse(code = 201, message = "Requisição bem sucedida"),
+						@ApiResponse(code = 400, message = "Requisição não atendida dados incorretos ou falta informações"),
+						@ApiResponse(code = 500, message = "Sistema indisponivel")})
 	
 	public ResponseEntity<ProductDtoResponse> create(@Valid @RequestBody ProductDtoRequest produto){
 			ProductDtoResponse produtoResponse =  productService.save(produto);
@@ -69,20 +58,11 @@ public class ProductController {
 	}
 	
 	@GetMapping
-	@ApiOperation(
-				httpMethod = "GET",
-				value = "Listar todos os produtos cadastrados na base de dados",
-				response = ProductDtoResponse.class,
-				nickname = "findAllProducts",
-				notes="Não há parametros a serem repassados na URI, somente uma requisição do tipo GET,"
-						+ "\n fará com que obtenha a listagem de todos os produtos cadastrados na base de dados"
-			
-			)
+	@ApiOperation(value = "Exibe todos os produtos cadastrados")
 	@ApiResponses(value = {
-					@ApiResponse(code = 200, message = "ok, Requisição bem sucedida"),
-					@ApiResponse(code = 404, message = "Not Found, recurso não encontrado")
-	}
-			)
+					@ApiResponse(code = 200, message = "Requisição bem sucedida"),
+					@ApiResponse(code = 404, message = "Recurso não encontrado"),
+					@ApiResponse(code = 500, message = "Sistema indisponivel")})
 	
 	public ResponseEntity<List<ProductDtoResponse>> getAll(){
 		List<ProductDtoResponse> produtoResponse = productService.findAll();
@@ -90,44 +70,27 @@ public class ProductController {
 	}
 	
 	@GetMapping("/{id}")
-	@ApiOperation(
-				httpMethod = "GET",
-				value = "Listar um produto especifico através de seu identificador ID",
-				response = ProductDtoResponse.class,
-				nickname = "getByIdProduct",
-				notes="Como parametro na URI deverá ser repassado um ID de um produto especifico para que seja feito a consulta"
-						+ "\n na base de dados, como resposta caso ID seja valido será exibido os dados do produto consultado"
-						+ "\n no corpo da resposta, a resposta estará no formado Json"
-			)
+	@ApiOperation(value = "Exibe um produto atraves de um id válido")
 	@ApiResponses(value= {
-				@ApiResponse(code=200, message = "ok, requisição bem sucedida"),
-				@ApiResponse(code = 404, message = "Not Found, recurso não encontrado")
-	}
-			)
+				@ApiResponse(code = 200, message = "Requisição bem sucedida"),
+				@ApiResponse(code = 400, message = "Requisição não atendida, dados incorretos ou falta informações"),
+				@ApiResponse(code = 404, message = "Produto não encontrado")})
 	
-	public ResponseEntity<ProductDtoResponse> getById(@PathVariable @ApiParam("id do produto") Long id ){
+	public ResponseEntity<ProductDtoResponse> getById(@PathVariable Long id ){
 		ProductDtoResponse produtoResponse = productService.findById(id);
 		return ResponseEntity.ok().body(produtoResponse);
 	}
 	
 	@PutMapping("/{id}")
 	@Transactional
-	@ApiOperation(
-			httpMethod = "PUT",
-			value = "Atualizar informações de um produto através de um id informado",
-			response = ProductDtoResponse.class,
-			nickname = "updateProduct",
-			notes="Os dados de um produto podem ser atualizado como descrição e status, para fazer a atualização "
-					+ "\n deverá ser informado o id do produto que receberá a atualização, o id deverá ser repassado"
-					+ "\n na URI do endpoint"
-			)
+	@ApiOperation(value = "Atualizar informações de um produto")
 	@ApiResponses(value = {
-					@ApiResponse(code = 200, message = "ok, requisição bem sucedida" ),
-					@ApiResponse(code = 404, message = "Not Found, recusro não encontrado")
-	}
-			)
+					@ApiResponse(code = 200, message = "Requisição bem sucedida" ),
+					@ApiResponse(code = 400, message = "Requisição não atendida, dados incorretos ou falta informações"),
+					@ApiResponse(code = 404, message = "Produto não encontrado"),
+					@ApiResponse(code = 500, message = "Sistema indisponivel")})
 	
-	public ResponseEntity<ProductDtoResponse> update(@Valid @PathVariable Long id, @ApiParam("id do produto")
+	public ResponseEntity<ProductDtoResponse> update(@Valid @PathVariable Long id,
 			@RequestBody ProductDtoRequestUpdate productUpdate){
 		ProductDtoResponse productResponse = productService.update(id, productUpdate);
 		this.rabbitmqService.enviaMensagem(RabbitmqConstantes.FILA_PRODUCT, productResponse);
@@ -136,20 +99,13 @@ public class ProductController {
 	}
 	@DeleteMapping("/{id}")
 	@Transactional
-	@ApiOperation(
-			httpMethod = "DELETE",
-			value = "Excluir um produto da base de dados.",
-			nickname = "deleteProduct",
-			notes = "Um id de um produto deverá ser informado na URI para que seja excluido,"
-					+ "\n o ID sendo válido a exclusão será efetuada com sucesso"
-			)
+	@ApiOperation(value = "Excluir um produto da base de dados.")
 	@ApiResponses(value= {
-						@ApiResponse(code = 204, message = "No Content, requisição ben sucedida, não há recurso a ser exibido"),
-						@ApiResponse(code = 404, message = "Not Found, recurso não encontrado" )
-						
-	}
-			)
-	public ResponseEntity<?> delete(@PathVariable @ApiParam("id do produto") Long id){
+						@ApiResponse(code = 204, message = "Requisição bem sucedida, não há recurso a ser exibido"),
+						@ApiResponse(code = 404, message = "Produto não encontrado"),
+						@ApiResponse(code = 500, message = "Sistema indisponivel")})
+	
+	public ResponseEntity<?> delete(@PathVariable Long id){
 		productService.delete(id);
 		return ResponseEntity.noContent().build();
 	}
