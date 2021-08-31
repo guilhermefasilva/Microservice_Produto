63ce5c9a3a7d3ef7e0f962b8397aa768e3e6aa6d
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-08-31 14:07:59 -0300

    Testes unitários, possiveis cenarios de exception

--- src/test/java/io/guilhermefasilva/microservice/product/service/ProductServiceTest.java
+++ src/test/java/io/guilhermefasilva/microservice/product/service/ProductServiceTest.java
@@ -1,6 +1,9 @@
 package io.guilhermefasilva.microservice.product.service;
 
 
+
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
@@ -54,84 +57,102 @@ class ProductServiceTest {
 	
 	
 	
-	
-	
-	
 	@Test
-	public void deveSalvarUmproduto() {
+	public void validate_SaveProduct_ExpectedSucess() {
 			//cenario
-		    when(this.productRepository.save((Product) any())).thenReturn(produto);
-		   
+		    when(this.productRepository.save(any())).thenReturn(produto);
 		    //ação
 		    this.productService.save(productRequest);
-		    
 		    //verificação
-		    verify(this.productRepository, times(1)).save((Product) any());
-		
-
+		    verify(this.productRepository, times(1)).save(any());
 	}
 	
 	@Test
-	public void deveRetornarumProdutoPorId() {
+	public void validate_ProductFind_ExpectedSucess() {
 		
 		//Cenario
-		Optional<Product> product = Optional.<Product>of(produto);
-		when(productRepository.findById((Long) any())).thenReturn(product);
-		
+		Optional<Product> product = Optional.of(produto);
+		when(productRepository.findById(any())).thenReturn(product);
 		//Ação
 		this.productService.findById(any());
-		
 		//verificação
-		verify(this.productRepository, times(1)).findById((Long)any());
+		verify(this.productRepository, times(1)).findById(any());
 	}
 	
 	@Test
-	public void deveAtualizarUmProdutoPassandoId() {
+	public void validate_ProductNotFind_ExpectedThrownException() {
 		//cenario
-		Optional<Product> productOptional = Optional.<Product>of(produto);
-		when(productRepository.findById((Long) any())).thenReturn(productOptional).thenThrow(ResourceNotFoundException.class);
-		when(productRepository.save((Product) any())).thenReturn(produto);
-		
+		when(productRepository.findById(any())).thenThrow(ResourceNotFoundException.class);
 		//ação
-		this.productService.update(any(), productUpdate);
-		
+		assertThatThrownBy(()->productService.findById(any()))
+							.isInstanceOf(RuntimeException.class)
+							.withFailMessage("Resource not found. Id: ",produto.getId());
 		//verificação
-		verify(this.productRepository, times(1)).save((Product) any());
+			verify(this.productRepository, times(1)).findById(any());
+							
+	}
+	
+	@Test
+	public void validate_UpdateProductFindById_ExpectedSucess() {
+		//contemplar 2 cenrarios
+		//cenario
+		Optional<Product> productOptional = Optional.of(produto);
+		when(productRepository.findById(any(Long.class))).thenReturn(productOptional);
+		when(productRepository.save(any(Product.class))).thenReturn(produto);
+		//ação
+		this.productService.update(any(Long.class), productUpdate);
+		//verificação
+		verify(this.productRepository, times(1)).save(any(Product.class));
 		
 	}
 	
 	@Test
-	public void deveRetornarUmaListaDeProdutos() {
+	public void validate_ProductUpdateNotFindById_ExpectedThrownException() {
+		//cenario
+		when(productRepository.findById(any(Long.class))).thenThrow(ResourceNotFoundException.class);
+		//ação
+		assertThatThrownBy(()->productService.update(any(Long.class), productUpdate))
+							.isInstanceOf(RuntimeException.class)
+							.withFailMessage("Resource not found. Id:",produto.getId());
+		//verificação
+		verify(this.productRepository, times(1)).findById(any(Long.class));
+		
+	}
+	
+	@Test
+	public void validate_ProductAllList_ExpectedSucess() {
 		//cenario
 		when(this.productRepository.findByNome(any(), any())).thenReturn(productPage);
-		
 		//ação
 		this.productService.findAll(any(),any());
-	
 		//verificação
 		verify(this.productRepository, times(1)).findByNome(any(), any());
 		
 	}
 	
 	@Test
-	public void deveDeletarUmProduto() {
+	public void validate_ProductDeleteFindById_ExpectedSucess() {
 		//cenario
-		Optional<Product> productOptional = Optional.<Product>of(produto);
-		when(productRepository.findById((Long)any())).thenReturn(productOptional).thenThrow(ResourceNotFoundException.class);
-		doNothing().when(productRepository).delete((Product)any());
-		
+		Optional<Product> productOptional = Optional.of(produto);
+		when(productRepository.findById(any())).thenReturn(productOptional);
+		doNothing().when(productRepository).delete(any(Product.class));
 		//ação
 		this.productService.delete(any());
 		this.rabbitDeleteQueue.sendMessage(productOptional);
-		
 		//verify
-		verify(this.productRepository, times(1)).delete((Product)any());
+		verify(this.productRepository, times(1)).delete(any(Product.class));
 		verify(this.rabbitDeleteQueue, times(1)).sendMessage(productOptional);
-		
-		
 	}
-
 	
-	
-
+	@Test
+	public void validate_ProductNotFindById_ExpectedThrownException() {
+		//cenario
+		when(productRepository.findById(any(Long.class))).thenThrow(ResourceNotFoundException.class);
+		//ação
+		assertThatThrownBy(()->productService.delete(any(Long.class)))
+							.isInstanceOf(RuntimeException.class)
+							.withFailMessage("Resource not found. Id:",produto.getId());
+		//verificação
+		verify(this.productRepository, times(1)).findById(any(Long.class));
+	}
 }
