d3f1b39df86329ed57bc4e67f23e7d789679fdc0
Guilherme Assis <guilherme.assis@compasso.com.br>
2021-09-01 10:20:25 -0300

    Refactoring em testes

--- src/main/java/io/guilhermefasilva/microservice/product/service/ProductService.java
+++ src/main/java/io/guilhermefasilva/microservice/product/service/ProductService.java
@@ -71,5 +71,6 @@ public class ProductService  {
 		this.rabbitmqDeleteQueueSender.sendMessage(produto);
 		
 	}
+	
 	 
 }
--- src/test/java/io/guilhermefasilva/microservice/product/service/ProductServiceTest.java
+++ src/test/java/io/guilhermefasilva/microservice/product/service/ProductServiceTest.java
@@ -59,100 +59,105 @@ class ProductServiceTest {
 	
 	@Test
 	public void validate_SaveProduct_ExpectedSucess() {
-			//cenario
 		    when(this.productRepository.save(any())).thenReturn(produto);
-		    //ação
+		   
 		    this.productService.save(productRequest);
-		    //verificação
+		   
 		    verify(this.productRepository, times(1)).save(any());
 	}
 	
 	@Test
 	public void validate_ProductFind_ExpectedSucess() {
 		
-		//Cenario
 		Optional<Product> product = Optional.of(produto);
 		when(productRepository.findById(any())).thenReturn(product);
-		//Ação
+		
 		this.productService.findById(any());
-		//verificação
+		
 		verify(this.productRepository, times(1)).findById(any());
 	}
 	
-	@Test
-	public void validate_ProductNotFind_ExpectedThrownException() {
-		//cenario
-		when(productRepository.findById(any())).thenThrow(ResourceNotFoundException.class);
-		//ação
-		assertThatThrownBy(()->productService.findById(any()))
-							.isInstanceOf(RuntimeException.class)
-							.withFailMessage("Resource not found. Id: ",produto.getId());
-		//verificação
-			verify(this.productRepository, times(1)).findById(any());
-							
-	}
+	
 	
 	@Test
 	public void validate_UpdateProductFindById_ExpectedSucess() {
-		//contemplar 2 cenrarios
-		//cenario
+
 		Optional<Product> productOptional = Optional.of(produto);
 		when(productRepository.findById(any(Long.class))).thenReturn(productOptional);
 		when(productRepository.save(any(Product.class))).thenReturn(produto);
-		//ação
+
 		this.productService.update(any(Long.class), productUpdate);
-		//verificação
+
 		verify(this.productRepository, times(1)).save(any(Product.class));
 		
 	}
 	
-	@Test
-	public void validate_ProductUpdateNotFindById_ExpectedThrownException() {
-		//cenario
-		when(productRepository.findById(any(Long.class))).thenThrow(ResourceNotFoundException.class);
-		//ação
-		assertThatThrownBy(()->productService.update(any(Long.class), productUpdate))
-							.isInstanceOf(RuntimeException.class)
-							.withFailMessage("Resource not found. Id:",produto.getId());
-		//verificação
-		verify(this.productRepository, times(1)).findById(any(Long.class));
-		
-	}
+	
 	
 	@Test
 	public void validate_ProductAllList_ExpectedSucess() {
-		//cenario
+
 		when(this.productRepository.findByNome(any(), any())).thenReturn(productPage);
-		//ação
+
 		this.productService.findAll(any(),any());
-		//verificação
+
 		verify(this.productRepository, times(1)).findByNome(any(), any());
 		
 	}
 	
 	@Test
 	public void validate_ProductDeleteFindById_ExpectedSucess() {
-		//cenario
+
 		Optional<Product> productOptional = Optional.of(produto);
 		when(productRepository.findById(any())).thenReturn(productOptional);
 		doNothing().when(productRepository).delete(any(Product.class));
-		//ação
+
 		this.productService.delete(any());
 		this.rabbitDeleteQueue.sendMessage(productOptional);
-		//verify
+
 		verify(this.productRepository, times(1)).delete(any(Product.class));
 		verify(this.rabbitDeleteQueue, times(1)).sendMessage(productOptional);
 	}
 	
 	@Test
-	public void validate_ProductNotFindById_ExpectedThrownException() {
-		//cenario
+	public void validate_ProductDeleteNotFindById_ExpectedThrownException() {
+ 
 		when(productRepository.findById(any(Long.class))).thenThrow(ResourceNotFoundException.class);
-		//ação
+ 
 		assertThatThrownBy(()->productService.delete(any(Long.class)))
 							.isInstanceOf(RuntimeException.class)
 							.withFailMessage("Resource not found. Id:",produto.getId());
-		//verificação
+
 		verify(this.productRepository, times(1)).findById(any(Long.class));
 	}
+	
+	
+	@Test
+	public void validate_ProductNotFindById_ExpectedThrownException() {
+
+		when(productRepository.findById(any())).thenThrow(ResourceNotFoundException.class);
+
+		assertThatThrownBy(()->productService.findById(any()))
+							.isInstanceOf(RuntimeException.class)
+							.withFailMessage("Resource not found. Id: ",produto.getId());
+
+		verify(this.productRepository, times(1)).findById(any());
+							
+	}
+	
+	@Test
+	public void validate_ProductUpdateNotFindById_ExpectedThrownException() {
+		
+		when(productRepository.findById(any(Long.class))).thenThrow(ResourceNotFoundException.class);
+
+		assertThatThrownBy(()->productService.update(any(Long.class), productUpdate))
+							.isInstanceOf(RuntimeException.class)
+							.withFailMessage("Resource not found. Id:",produto.getId());
+
+		verify(this.productRepository, times(1)).findById(any(Long.class));
+		
+	}
+	
+	
+	
 }
